0aba13b327917355b9ea4fc3ed6331838d74bae3
Roger Vacovsky <rvacovsky@gmail.com>
2022-06-03 15:23:11 -0400

    created associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'user_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'user_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
 
 module.exports = {
   Product,
